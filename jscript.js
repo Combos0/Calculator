1445afbda5c288c829b0ba48c60798d29190da5e
Combos0 <jcx429.sc2@gmail.com>
2022-07-15 16:29:44 -0400

    added basic css numpad now enters numbers into display

---
+++ jscript.js
@@ -0,0 +1,88 @@
+let inputs = [];
+
+const outputDisplay = document.getElementById('output-space');
+outputDisplay.textContent = 0;
+function updatesOutput() {
+    outputDisplay.textContent = inputs;
+};
+
+const numberPad = {
+    zero: document.getElementById('button0'),
+    one: document.getElementById('button1'),
+    two: document.getElementById('button2'),
+    three: document.getElementById('button3'),
+    four: document.getElementById('button4'),
+    five: document.getElementById('button5'),
+    six: document.getElementById('button6'),
+    seven: document.getElementById('button7'),
+    eight: document.getElementById('button8'),
+    nine: document.getElementById('button9'),
+};
+
+const buttonOperators = {
+    plus: document.getElementById('operatorPlus'),
+    minus: document.getElementById('operatorMinus'),
+    multiply: document.getElementById('operatorMultiply'),
+    divide: document.getElementById('operatorDivide'),
+    enter: document.getElementById('operatorEnter'),
+};
+
+numberPad.one.addEventListener('click', () => {
+    let newInput = 1;
+    inputs.push(newInput);
+    updatesOutput();
+});
+
+numberPad.two.addEventListener('click', () => {
+    let newInput = 2;
+    inputs.push(newInput);
+    updatesOutput();
+});
+
+numberPad.three.addEventListener('click', () => {
+    let newInput = 3;
+    inputs.push(newInput);
+    updatesOutput();
+});
+
+numberPad.four.addEventListener('click', () => {
+    let newInput = 4;
+    inputs.push(newInput);
+    updatesOutput();
+});
+
+numberPad.five.addEventListener('click', () => {
+    let newInput = 5;
+    inputs.push(newInput);
+    updatesOutput();
+});
+
+numberPad.six.addEventListener('click', () => {
+    let newInput = 6;
+    inputs.push(newInput);
+    updatesOutput();
+});
+
+numberPad.seven.addEventListener('click', () => {
+    let newInput = 7;
+    inputs.push(newInput);
+    updatesOutput();
+});
+
+numberPad.eight.addEventListener('click', () => {
+    let newInput = 8;
+    inputs.push(newInput);
+    updatesOutput();
+});
+
+numberPad.nine.addEventListener('click', () => {
+    let newInput = 9;
+    inputs.push(newInput);
+    updatesOutput();
+});
+
+numberPad.zero.addEventListener('click', () => {
+    let newInput = 0;
+    inputs.push(newInput);
+    updatesOutput();
+});
